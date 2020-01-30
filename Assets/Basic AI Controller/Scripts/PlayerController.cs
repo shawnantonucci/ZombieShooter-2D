9dd6e98c3d8535060b30c872bb5ba75370e3b37f
Shawn Antonucci <shawnantonucci2018@gmail.com>
2020-01-30 01:19:38 -0500

    setup repo

---
+++ Assets/Basic AI Controller/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 7.0f;
+    float rotationSpeed = 100.0f;
+    Animator m_Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = (Input.GetAxis("Vertical") * speed) * Time.deltaTime;
+        float rotation = (Input.GetAxis("Horizontal") * rotationSpeed) * Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_Animator.SetBool("isWalking", true);
+        } else {
+            m_Animator.SetBool("isWalking", false);
+        }
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            m_Animator.SetBool("isAttacking", true);
+        } else {
+            m_Animator.SetBool("isAttacking", false);
+        }
+    }
+
+    public void ReceiveDamage(float damage)
+    {
+        Debug.Log("PLAYER_CONTROLLER: Player hit with " + damage + " damage!");
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootEnemy();
+        }
+    }
+
+    private void ShootEnemy()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit))
+        {
+            print("Found an object - distance: " + hit.distance);
+            Destroy(hit.transform.gameObject);
+        }
+    }
+}
